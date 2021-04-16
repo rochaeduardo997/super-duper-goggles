5692302e92fd87275f0cc36fdefb370bb4f7d2c1
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-04-15 21:20:11 -0300

    Fixed bugs and created user authentication to create, delete and update post

---
+++ server/middleware/UserAuth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const secret = "thisisasecretok";
+
+module.exports = (req, res, next) => {
+  const authToken = req.headers['authorization'];
+
+  if (authToken != undefined) {
+    const bearer = authToken.split(' ');
+    const token = bearer[1];
+    
+    const decoded = jwt.verify(token, secret);
+
+    if (decoded.role > 0) {
+      return next();
+    } else {
+      return res.status(400).json({ error: "You don\'t have authentication for this" });
+    }
+  } else {
+    return res.status(400).json({ error: "Failed on authentication" });
+  }
+}

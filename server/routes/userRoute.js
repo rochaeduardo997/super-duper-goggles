58ed6cad2ea52d14b8f805f983c206f4c0d4d77c
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-04-21 21:51:46 -0300

    Removed route authentication for create a new user

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -9,7 +9,7 @@ const AdminAuth = require("../middleware/AdminAuth");
 
 router.get("/users", AdminAuth, UserController.index);
 router.get("/user/:id", AdminAuth, UserController.findById);
-router.post("/user", AdminAuth, UserController.createNew);
+router.post("/user", UserController.createNew);
 router.delete("/user/:id", AdminAuth, UserController.deleteById);
 router.put("/user/:id", AdminAuth, UserController.updateUser);
 router.post("/login", UserController.loginUser);

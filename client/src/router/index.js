96a4f6fdcb4f7c2aada9a8b1b30f504e6a75c14e
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-04-22 08:09:48 -0300

    Created view and route for login, storing authentication token in local storage

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/cadastro',
     name: 'Cadastro',
     component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
---
+++ client/src/views/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <h2 class="title">Login</h2>
+    <div class="columns is-mobile is-centered">
+      <div class="card">
+        <div class="card-content">
+          <div class="field">
+            <label class="label">E-mail</label>
+            <div class="control">
+              <input
+                class="input"
+                type="email"
+                placeholder="meuemail@dominio.com"
+                v-model="email"
+              />
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Senha</label>
+            <div class="control">
+              <input
+                class="input"
+                type="password"
+                placeholder="********"
+                v-model="password"
+              />
+            </div>
+          </div>
+
+          <hr />
+
+          <button class="button is-success" v-on:click="login()">Login</button>
+
+          <br />
+          <br />
+
+          <div class="notification is-danger" v-if="error != undefined">
+            <p>{{ error }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+
+      error: undefined,
+    };
+  },
+
+  methods: {
+    login() {
+      axios({
+        url: "http://localhost:5001/login",
+        method: "post",
+        data: {
+          email: this.email,
+          password: this.password,
+        },
+      })
+        .then((res) => {
+          const token = res.data.token;
+          localStorage.setItem('token', token)
+          this.$router.push({ name: "Home" });
+        })
+        .catch((error) => {
+          this.error = error.response.data.error;
+
+          console.log(this.error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>

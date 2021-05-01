459fe01d0aadd4405917983469b1520e7f9aed56
rochaeduardo997 <rochaeduardo997@protonmail.com>
2021-05-01 17:47:36 -0300

    Home now shows posts components

---
+++ client/src/components/PostsComponent.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div v-for="post in allPosts" :key="post.id" class="card">
+      <div class="card-content">
+        <article class="media">
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>{{ post.title }}</strong> <br />
+                <br />
+                <small>{{ post.username }}</small> &nbsp;
+                <small>{{ post.createdAt }}</small>
+              </p>
+            </div>
+            <hr />
+            <p>
+              <button
+                v-on:click="
+                  activeReadModal(post.title);
+                  this.isPostReadModal = !this.isPostReadModal;
+                "
+                class="button is-success is-rounded"
+              >
+                Leia
+              </button>
+            </p>
+          </div>
+        </article>
+      </div>
+      <hr />
+    </div>
+
+    <!-- Read modal -->
+    <div v-show="isPostReadModal != false" class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{ readPostTitle }}</p>
+          <button
+            class="delete"
+            aria-label="close"
+            v-on:click="this.isPostReadModal = !this.isPostReadModal"
+          ></button>
+        </header>
+        <section class="modal-card-body">
+          <!-- Content ... -->
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      allPosts: [],
+
+      readPostTitle: "",
+      isPostReadModal: false,
+    };
+  },
+
+  created() {
+    axios({
+      url: "http://localhost:5001/posts",
+      method: "get",
+    })
+      .then((res) => {
+        this.allPosts = res.data.result;
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  },
+
+  methods: {
+    activeReadModal(readPostTitle) {
+      console.log(readPostTitle);
+      this.readPostTitle = readPostTitle;
+    },
+  },
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <PostsComponent />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostsComponent from "../components/PostsComponent";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { PostsComponent },
+};
 </script>
